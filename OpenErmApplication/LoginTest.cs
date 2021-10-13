4334d28778902b5f31a49625cbd74563669daf54
balaji-githubstore <balaji0017@gmail.com>
2021-10-13 16:44:56 +0530

    testcase attribute for data driven

--- OpenErmApplication/LoginTest.cs
+++ OpenErmApplication/LoginTest.cs
@@ -25,19 +25,20 @@ namespace Zensoft.OpenErmApplication
             Assert.AreEqual("Invalid username or password", actualValue);
         }
 
-
+        [TestCase("admin", "pass", "English (Indian)", "OpenEMR")]
+        [TestCase("physician", "physician", "Dutch", "OpenEMR")]
         [Test]
-        public void ValidCredentialTest()
+        public void ValidCredentialTest(string username,string password,string language,string expectedValue)
         {
-            LoginPage.EnterUsername(driver, "admin");
-            LoginPage.EnterPassword(driver, "pass");
-            LoginPage.SelectLanguageByText(driver, "English (Indian)");
+            LoginPage.EnterUsername(driver, username);
+            LoginPage.EnterPassword(driver, password);
+            LoginPage.SelectLanguageByText(driver, language);
             LoginPage.ClickOnLogin(driver);
 
             MainPage.WaitForPresenceOfCalendar(driver);
             string actualValue = MainPage.GetMainPageTitle(driver);    
             
-            Assert.AreEqual("OpenEMR", actualValue);
+            Assert.AreEqual(expectedValue, actualValue);
         }
     }
 }
