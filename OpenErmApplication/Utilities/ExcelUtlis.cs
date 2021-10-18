2f6bf95e3657bbedc2d949f8167982be19f4573b
balaji-githubstore <balaji0017@gmail.com>
2021-10-18 09:42:04 +0530

    created ConvertSheetIntoObjectArray

---
+++ OpenErmApplication/Utilities/ExcelUtlis.cs
@@ -0,0 +1,37 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zensoft.OpenErmApplication.Utilities
+{
+    class ExcelUtlis
+    {
+        public static object[] ConvertSheetIntoObjectArray(string file,string sheetname)
+        {
+            using (XLWorkbook book = new XLWorkbook(file))
+            {
+                var sheet = book.Worksheet(sheetname);
+                var range = sheet.RangeUsed();
+
+                int rowCount = range.RowCount();
+                int colCount = range.ColumnCount();
+
+                object[] main = new object[rowCount - 1];
+
+                for (int r = 2; r <= rowCount; r++)
+                {
+                    object[] temp = new object[colCount];
+                    for (int c = 1; c <= colCount; c++)
+                    {
+                        string value = range.Cell(r, c).GetString();
+                        temp[c - 1] = value;
+                    }
+                    main[r - 2] = temp;
+                }
+                return main;
+            }
+        }
+
+    }
+}
